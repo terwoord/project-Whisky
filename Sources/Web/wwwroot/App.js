25a7f0cde309a7c3504ef471a7fcc83f966b35c1
Matthijs ter Woord <matthijsterwoord@gmail.com>
2017-08-06 16:25:00 +0200

    .

--- Sources/Web/wwwroot/App.js
+++ Sources/Web/wwwroot/App.js
@@ -88,11 +88,13 @@ System.register("App", ["Locations"], function (exports_5, context_5) {
         ],
         execute: function () {
             (function (App) {
+                var xInfoWindow;
                 function initialize() {
                     App.map = new google.maps.Map(document.getElementById('content'), {
                         center: { lat: 57.63712860378047, lng: -4.570843749999991 },
                         zoom: 8
                     });
+                    xInfoWindow = new google.maps.InfoWindow();
                     var xLocations = Locations_1.Locations.getLocations();
                     buildLocationList(xLocations);
                     addLocationsToGoogle(xLocations);
@@ -110,11 +112,27 @@ System.register("App", ["Locations"], function (exports_5, context_5) {
                     }
                 }
                 function addLocationsToGoogle(locations) {
-                    for (var xLocation of locations) {
-                        var xMarker = new google.maps.Marker({
+                    for (let xLocation of locations) {
+                        let xMarker = new google.maps.Marker({
                             position: xLocation.coordinate,
                             label: xLocation.kind[0],
-                            map: App.map,
+                            map: App.map
+                        });
+                        xMarker.addListener('click', () => {
+                            var xTitle;
+                            if (xLocation.url) {
+                                xTitle = `<a id="title" href="${xLocation.url}" target="_blank">${xLocation.title}</a>`;
+                            }
+                            else {
+                                xTitle = `<span id="title">${xLocation.title}</span>`;
+                            }
+                            xInfoWindow.setContent(`<div id="infoContent">
+${xTitle}<br>
+<img src="${xLocation.image}"><br>
+<span id="infoContentDescription">${xLocation.description || ""}</span>
+</div>`);
+                            xInfoWindow.close();
+                            xInfoWindow.open(App.map, xMarker);
                         });
                     }
                 }
--- Sources/Web/wwwroot/App/App.ts
+++ Sources/Web/wwwroot/App/App.ts
@@ -5,12 +5,16 @@ import {ILocationInfo} from "./ILocationInfo";
 export namespace App {
     export var map: google.maps.Map;
 
+    var xInfoWindow: google.maps.InfoWindow;
+
     export function initialize() {
         map = new google.maps.Map(document.getElementById('content'),
             {
                 center: { lat: 57.63712860378047, lng: -4.570843749999991 },
                 zoom: 8
             });
+
+        xInfoWindow = new google.maps.InfoWindow();
 
         var xLocations = Locations.getLocations();
         buildLocationList(xLocations);
@@ -30,13 +34,30 @@ export namespace App {
     }
 
     function addLocationsToGoogle(locations: ILocationInfo[]) {
-        for (var xLocation of locations) {
-            var xMarker = new google.maps.Marker({
+        for (let xLocation of locations) {
+            let xMarker = new google.maps.Marker({
                 position: xLocation.coordinate,
                 label: xLocation.kind[0],
-                map: map,
-
+                map: map
             });
+
+            xMarker.addListener('click',
+                () => {
+                    var xTitle: string;
+                    if (xLocation.url) {
+                        xTitle = `<a id="title" href="${xLocation.url}" target="_blank">${xLocation.title}</a>`;
+                    } else {
+                        xTitle = `<span id="title">${xLocation.title}</span>`;
+                    }
+
+                    xInfoWindow.setContent(`<div id="infoContent">
+${xTitle}<br>
+<img src="${xLocation.image}"><br>
+<span id="infoContentDescription">${xLocation.description || ""}</span>
+</div>`);
+                    xInfoWindow.close();
+                    xInfoWindow.open(map, xMarker);
+                });
         }
     }
 }
